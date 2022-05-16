cd75dc610455ea22fb298fa176cafa89522a0c5b
Aris Vayas <aris.vayas@gmail.com>
2022-05-15 20:14:11 -0400

    commit tree e3d904277be356e8e4ebc2d52f15c93dd519e075

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
 # Your code here!
+def greet_programmer(name="programmer")
+    puts "Hello, #{name}!"
+end
+def greet name
+    puts "Hello, #{name}!"
+end
+greet_programmer
+greet("Naureen")
+greet("Jimmy")
+
+def add num1, num2
+    num1+num2 
+end
+def halve num
+    puts num/2 
+
+end
+# add 1,2
+halve()
